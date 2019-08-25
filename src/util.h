2194689dd6b0309d142aacacbc40438e299362c2
songcap <18741872667@163.com>
2019-08-26 00:12:51 +0800

    改了一下编译选项的bug

--- src/util.h
+++ src/util.h
@@ -1,8 +1,10 @@
-#ifdef  __UTIL_H__
-#define __UTIL_H__
+#ifndef    __UTIL_H__
+#define   __UTIL_H__
+
 #include <pthread.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 
 namespace mysrv{
 
